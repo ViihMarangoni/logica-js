fcb8673e80b5ee58f0488bb45e92ee071ca74bc6
ViihMarangoni <142364559+ViihMarangoni@users.noreply.github.com>
2023-11-13 23:20:33 +0000

    laço enquanto - whilw

---
+++ atividade01.js
@@ -0,0 +1,19 @@
+// Laço Enquanto
+
+let contador = 1
+while(contador <= 10){
+    console.log(contador) // conta do 1 ao 10
+    contador++
+}
+
+while(contador >= 1){
+    console.log(contador) // conta do 10 ao 0
+    contador--
+}
+
+
+let i = 1
+while(i <= 1){
+    console.log("Botafogo Campeão")
+    i++
+}
