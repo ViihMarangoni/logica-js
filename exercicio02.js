6a11906401e4c66b69385c1acf6a118a45a01456
ViihMarangoni <142364559+ViihMarangoni@users.noreply.github.com>
2023-11-14 01:14:46 +0000

    exercicios

---
+++ exercicio02.js
@@ -0,0 +1,7 @@
+// Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
+let numero = 1
+for (let numero = 1; numero <= 50; numero++) {
+    if(numero % 2 != 0) {
+        console.log(numero);
+    }
+}
