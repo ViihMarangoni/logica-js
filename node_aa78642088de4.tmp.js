71f25a4c46b5c212700514e60f4351946c8a6aac
ViihMarangoni <142364559+ViihMarangoni@users.noreply.github.com>
2023-11-14 22:42:01 +0000

    exercicio03

--- node_aa78642088de4.tmp.js
+++ node_aa78642088de4.tmp.js
@@ -1,9 +1,10 @@
 // Faça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles
 
 let inicio = 7
-let fim = 20
+let fim = 25
 
-while(inicio < fim-1){
-    inicio++
+while(inicio <= fim){
+    inicio+
     console.log(inicio)
+    
 }
