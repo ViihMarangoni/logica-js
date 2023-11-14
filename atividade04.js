031ff5c8bcdc962b0c0c1ed4eb503461c0826b9f
ViihMarangoni <142364559+ViihMarangoni@users.noreply.github.com>
2023-11-14 00:48:18 +0000

    Laço de repetição com array

---
+++ atividade04.js
@@ -0,0 +1,8 @@
+let frutas = ["🍇","🍓","🍊","🥑"]
+frutas.push("🍐")
+frutas.push("🍍")
+// i = incrementos - usamos para array
+console.dir(frutas)
+for(let i = 0; i<frutas.length ; i++){
+    console.log(frutas[i])
+} // length informa todas as posições do array
